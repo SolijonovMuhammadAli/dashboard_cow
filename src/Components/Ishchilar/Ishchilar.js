309f0c1a6a4eaf2ac0bb71efbaf83635062fc0c5
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-10-06 10:26:12 +0500

    update ishchilaar

--- src/Components/Ishchilar/Ishchilar.js
+++ src/Components/Ishchilar/Ishchilar.js
@@ -4,15 +4,26 @@ import Table from "./TableIshchilar";
 import { rows } from "./data";
 
 function Ishchilar() {
-  //const [familya, setFamilya] = useState("");
-  //const [name, setName] = useState("");
+  const [data, setData] = useState(rows);
+  const [familya, setFamilya] = useState("");
+  const [name, setName] = useState("");
 
   const handleChange1 = (e) => {
-    console.log(e.target.value);
+    setFamilya(e.target.value);
+    let newsRows = rows.filter((item) =>
+      item.person.familya.toUpperCase().includes(e.target.value.toUpperCase())
+    );
+    setData(newsRows);
+    setName("");
   };
-  // const handleChange2 = (e) => {
-  //   console.log(e.target.value);
-  // };
+  const handleChange2 = (e) => {
+    setName(e.target.value);
+    let newsRows = rows.filter((item) =>
+      item.person.name.toUpperCase().includes(e.target.value.toUpperCase())
+    );
+    setData(newsRows);
+    setFamilya("");
+  };
   return (
     <div>
       <Box
@@ -28,15 +39,17 @@ function Ishchilar() {
           onChange={handleChange1}
           label="Familya"
           variant="standard"
+          value={familya}
         />
         <TextField
           id="standard-basic"
-          //onChange={handleChange2}
+          onChange={handleChange2}
           label="Ism"
           variant="standard"
+          value={name}
         />
       </Box>
-      <Table rows={rows} />
+      <Table rows={data} />
     </div>
   );
 }
--- src/Components/Ishchilar/TableIshchilar.js
+++ src/Components/Ishchilar/TableIshchilar.js
@@ -57,152 +57,170 @@ export default function StickyHeadTable({ rows }) {
 
   return (
     <div>
-      <h1 className={style.tableheader}>
-        <Button variant="contained" color="primary">
-          Add
-        </Button>
-      </h1>
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead center="true">
-              <TableRow>
-                <TableCell align="center">Rasm</TableCell>
-                <TableCell align="center">Familyasi</TableCell>
-                <TableCell align="center">Ismi</TableCell>
-                <TableCell align="center">Ish haqqi</TableCell>
-                <TableCell align="center">Ish boshlagan vaqti</TableCell>
-                <TableCell align="center">To'liq ma'lumot</TableCell>
-                <TableCell align="center">O'zgartirish</TableCell>
-                <TableCell align="center">O'chirish</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => (
-                  <TableRow key={row.id}>
-                    <Modal show={open} onClose={handleClose}>
-                      <Modal.Header>
-                        <Modal.Title>{modal.person.name}</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                        <div>
-                          <b>Familya: </b>
-                          {modal.person.familya}
-                        </div>
-                        <div>
-                          <b>Ism: </b>
-                          {modal.person.name}
-                        </div>
-                        <div>
-                          <b>pasport number: </b>
-                          {modal.person.pasportserya}
-                        </div>
-                        <div>
-                          <b>Yashash manzili: </b>
-                          <ul>
-                            <li>
-                              <b>Viloyat: </b>
-                              {modal.person.yashash.viloyat}
-                            </li>
-                            <li>
-                              <b>Tuman: </b>
-                              {modal.person.yashash.tuman}
-                            </li>
-                            <li>
-                              <b>Mahalla: </b>
-                              {modal.person.yashash.mahalla}
-                            </li>
-                            <li>
-                              <b>Uy: </b>
-                              {modal.person.yashash.uy}
-                            </li>
-                          </ul>
-                        </div>
-                        <div>
-                          <b>Telephoe: </b>
-                          {modal.person.tel}
-                        </div>
-                        <div>
-                          <b>Tug'ligan sana: </b>
-                          {modal.person.brath}
-                        </div>
-                        <div>
-                          <b>Oyligi: </b>
-                          {modal.oyligi}
-                        </div>
-                        <div>
-                          <b>Kasbi: </b>
-                          {modal.kasbi}
-                        </div>
-                        <div>
-                          <b>Ish boshlagan vaqti: </b>
-                          {modal.ishga_kelgan_vaqti}
-                        </div>
-                      </Modal.Body>
-                      <Modal.Footer>
-                        <Button onClick={handleClose} color="error">
-                          Close
-                        </Button>
-                      </Modal.Footer>
-                    </Modal>
-                    <TableCell
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                      }}
-                    >
-                      <Avatar
-                        alt="Remy Sharp"
-                        src={`${row.image}`}
-                        sx={{ width: 56, height: 56 }}
-                      />
-                    </TableCell>
-                    <TableCell align="center">{row.person.familya}</TableCell>
-                    <TableCell align="center">{row.person.name}</TableCell>
-                    <TableCell align="center">{row.oyligi}</TableCell>
-                    <TableCell align="center">
-                      {row.ishga_kelgan_vaqti}
-                    </TableCell>
-                    <TableCell align="center">
-                      <Button
-                        onClick={() => handleOpen(row)}
-                        size="small"
-                        variant="contained"
-                        color="info"
-                      >
-                        Info
-                      </Button>
-                    </TableCell>
-                    <TableCell align="center">
-                      <Button size="small" variant="contained" color="primary">
-                        Edit
-                      </Button>
-                    </TableCell>
-                    <TableCell align="center">
-                      <ThemeProvider theme={theme}>
-                        <Button size="small" variant="contained" color="danger">
-                          Delete
-                        </Button>
-                      </ThemeProvider>
-                    </TableCell>
+      {rows.length > 0 ? (
+        <>
+          <h1 className={style.tableheader}>
+            <Button variant="contained" color="primary">
+              Add
+            </Button>
+          </h1>
+          <Paper sx={{ width: "100%", overflow: "hidden" }}>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead center="true">
+                  <TableRow>
+                    <TableCell align="center">Rasm</TableCell>
+                    <TableCell align="center">Familyasi</TableCell>
+                    <TableCell align="center">Ismi</TableCell>
+                    <TableCell align="center">Ish haqqi</TableCell>
+                    <TableCell align="center">Ish boshlagan vaqti</TableCell>
+                    <TableCell align="center">To'liq ma'lumot</TableCell>
+                    <TableCell align="center">O'zgartirish</TableCell>
+                    <TableCell align="center">O'chirish</TableCell>
                   </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
+                </TableHead>
+                <TableBody>
+                  {rows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => (
+                      <TableRow key={row.id}>
+                        <Modal show={open} onClose={handleClose}>
+                          <Modal.Header>
+                            <Modal.Title>{modal.person.name}</Modal.Title>
+                          </Modal.Header>
+                          <Modal.Body>
+                            <div>
+                              <b>Familya: </b>
+                              {modal.person.familya}
+                            </div>
+                            <div>
+                              <b>Ism: </b>
+                              {modal.person.name}
+                            </div>
+                            <div>
+                              <b>pasport number: </b>
+                              {modal.person.pasportserya}
+                            </div>
+                            <div>
+                              <b>Yashash manzili: </b>
+                              <ul>
+                                <li>
+                                  <b>Viloyat: </b>
+                                  {modal.person.yashash.viloyat}
+                                </li>
+                                <li>
+                                  <b>Tuman: </b>
+                                  {modal.person.yashash.tuman}
+                                </li>
+                                <li>
+                                  <b>Mahalla: </b>
+                                  {modal.person.yashash.mahalla}
+                                </li>
+                                <li>
+                                  <b>Uy: </b>
+                                  {modal.person.yashash.uy}
+                                </li>
+                              </ul>
+                            </div>
+                            <div>
+                              <b>Telephoe: </b>
+                              {modal.person.tel}
+                            </div>
+                            <div>
+                              <b>Tug'ligan sana: </b>
+                              {modal.person.brath}
+                            </div>
+                            <div>
+                              <b>Oyligi: </b>
+                              {modal.oyligi}
+                            </div>
+                            <div>
+                              <b>Kasbi: </b>
+                              {modal.kasbi}
+                            </div>
+                            <div>
+                              <b>Ish boshlagan vaqti: </b>
+                              {modal.ishga_kelgan_vaqti}
+                            </div>
+                          </Modal.Body>
+                          <Modal.Footer>
+                            <Button onClick={handleClose} color="error">
+                              Close
+                            </Button>
+                          </Modal.Footer>
+                        </Modal>
+                        <TableCell
+                          style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                        >
+                          <Avatar
+                            alt="Remy Sharp"
+                            src={`${row.image}`}
+                            sx={{ width: 56, height: 56 }}
+                          />
+                        </TableCell>
+                        <TableCell align="center">
+                          {row.person.familya}
+                        </TableCell>
+                        <TableCell align="center">{row.person.name}</TableCell>
+                        <TableCell align="center">{row.oyligi}</TableCell>
+                        <TableCell align="center">
+                          {row.ishga_kelgan_vaqti}
+                        </TableCell>
+                        <TableCell align="center">
+                          <Button
+                            onClick={() => handleOpen(row)}
+                            size="small"
+                            variant="contained"
+                            color="info"
+                          >
+                            Info
+                          </Button>
+                        </TableCell>
+                        <TableCell align="center">
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                          >
+                            Edit
+                          </Button>
+                        </TableCell>
+                        <TableCell align="center">
+                          <ThemeProvider theme={theme}>
+                            <Button
+                              size="small"
+                              variant="contained"
+                              color="danger"
+                            >
+                              Delete
+                            </Button>
+                          </ThemeProvider>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </>
+      ) : (
+        <h1 style={{ textAlign: "center", marginTop: "5%" }}>
+          Bunday ishchi mavjud emas
+        </h1>
+      )}
     </div>
   );
 }
